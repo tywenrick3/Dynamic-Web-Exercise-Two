b55b156e0d575efb23948d14576ea4804344ce63
Ty Wenrick <tew287@nyu.edu>
2021-09-28 08:52:47 -0400

    react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </switch>
+    </Router>
   );
 }
 
